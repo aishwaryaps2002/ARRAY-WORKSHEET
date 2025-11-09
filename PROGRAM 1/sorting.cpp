5bac5517cd5f6d7c57fc7e2aba7ace1daee329f4
aishwaryaps2002 <133131171+aishwaryaps2002@users.noreply.github.com>
2025-11-09 21:12:28 +0530

    sorting.cpp

---
+++ PROGRAM 1/sorting.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int n;
+  int arr[50];
+  cout << "Enter number of elements: ";
+  cin >> n;
+  cout << "Enter " << n << " elements:\n";
+  for(int i = 0; i < n; i++) 
+  {
+    cin >> arr[i];
+  }
+  for(int i = 0; i < n - 1; i++) 
+  {
+    for(int j = i + 1; j < n; j++) 
+    {
+      if(arr[i] > arr[j]) 
+      {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  cout << "\nArray after sorting :\n";
+  for(int i = 0; i < n; i++) 
+  {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
+
+    cout << endl;
+    return 0;
+}
